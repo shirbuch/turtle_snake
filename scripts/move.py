af34ab528f7968ea306797da824ea6724d5ed57a
shirbu <shirbuchner@yahoo.com>
2024-01-15 21:34:38 +0100

    Stop moving when reached wall

--- scripts/move.py
+++ scripts/move.py
@@ -9,21 +9,62 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
 current_pose = Pose()
-MAX_X = 11.096445
-MIN_X = -0.023555
-MAX_Y = 11.096445
-MIN_Y = -0.023555
+MAX_X = 11
+MIN_X = 0
+MAX_Y = 11
+MIN_Y = 0
 
-# todo: deleteme: distance <= 0 or (current_pose.x + distance >= MAX_X): # a check that we are good...
-# todo: stop if reached wall
+def print_turtle_pose(message=None):
+    print("----------------")
+    if message:
+        print(message)
+    print("x:", current_pose.x)
+    print("y:", current_pose.y)
+    print("theta:", current_pose.theta)
+    pose_degrees = current_pose.theta * 180/PI
+    print("pose_degrees:", pose_degrees)
+    print("----------------")
+PI = 3.1415926535897
+
+current_pose = Pose()
 
 def pose_callback(msg):
     global current_pose
     current_pose = msg
 
+# Wall facing checks
+def facing_lower_wall(pose_degrees):
+    return pose_degrees > 0 and pose_degrees < 180
+
+def facing_upper_wall(pose_degrees):
+    return pose_degrees > 180 and pose_degrees < 360
+
+def facing_right_wall(pose_degrees):
+    return pose_degrees > 90 and pose_degrees < 270
+
+def facing_left_wall(pose_degrees):
+    return (pose_degrees > 270 or pose_degrees < 90)
+
+# Bumping wall checks
+def bumping_lower_wall(pose_degrees, pose_y):
+    return facing_lower_wall(pose_degrees) and pose_y <= MIN_Y
+
+def bumping_upper_wall(pose_degrees, pose_y):
+    return facing_upper_wall(pose_degrees) and pose_y > MAX_Y
+
+def bumping_right_wall(pose_degrees, pose_x):
+    return facing_right_wall(pose_degrees) and pose_x > MAX_X
+
+def bumping_left_wall(pose_degrees, pose_x):
+    return facing_left_wall(pose_degrees) and pose_x <= MIN_X
+
+def bumping_wall(pose):
+    pose_degrees = 180 + pose.theta * 180/PI
+    return bumping_right_wall(pose_degrees, pose.x) or bumping_left_wall(pose_degrees, pose.x) or bumping_upper_wall(pose_degrees, pose.y) or bumping_lower_wall(pose_degrees, pose.y)
+
+# Service logic
 def move_turtle(distance, speed=3):
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) # publishing to the turtle velocity
-    rospy.Subscriber('/turtle1/pose', Pose, pose_callback) # subscribe to turtle position
     
     vel_msg = Twist()
     vel_msg.linear.x = speed # linear.x moves the turtle
@@ -32,6 +73,9 @@ def move_turtle(distance, speed=3):
     distance_traveled = 0
     
     while(distance_traveled < distance): # while loop, for our moving
+        if bumping_wall(current_pose):
+            print("Reached a wall!")
+            break
         velocity_publisher.publish(vel_msg) # important, this is responsible for the moving itself
         distance_traveled = sqrt((current_pose.x - init_pos.x)**2 + (current_pose.y - init_pos.y)**2) # calculating distance traveled
     
@@ -42,6 +86,8 @@ def move_turtle(distance, speed=3):
     return
 
 def handle_move(req):
+    rospy.Subscriber('/turtle1/pose', Pose, pose_callback) # subscribe to turtle position
+
     try:
         distance = req.distance
         if distance < 0 or distance >= MAX_X - MIN_X:
